efbd723445766975e8ee0fe7933a0d4e33083600
CLKO82 <marcello.binoto@einstein.edu.br>
2025-09-02 10:23:08 -0300

    Update exercicio1.m - Marcello Binoto

--- exercicio1.m
+++ exercicio1.m
@@ -1,15 +1,27 @@
-function t = exercicio1(func,x0)
+function t = exercicio01(func,func_d,x0)
 
 % nao alterar: inicio
 es = 1;
 imax = 20;
 % nao alterar: fim
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% digite seu codigo aqui
-t = 0; % alterar
+t_raizes = zeros(imax,1);
+t_raizes(1)  = x0;
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%
-
+for ii = 1:length(t_raizes)-1
+    if ii ~= 1
+        erro(ii) = abs((t_raizes(ii) - t_raizes(ii-1))/t_raizes(ii))
+        if erro(ii) < es
+            break
+        endif
+    endif
+        t_raizes(ii+1) = t_raizes(ii) - func(t_raizes(ii))/func_d(t_raizes(ii));    
+    
+    
+endfor
+t = t_raizes(ii);
+    
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 endfunction
