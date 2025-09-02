1b5122434b0b4dc357de4b877088a4e9a63c886f
CLKO82 <marcello.binoto@einstein.edu.br>
2025-09-02 10:28:52 -0300

    Update 2 exercicio1.m - Marcello Binoto

--- exercicio1.m
+++ exercicio1.m
@@ -1,27 +1,33 @@
-function t = exercicio01(func,func_d,x0)
+function t = exercicio1(func, x0)
 
-% nao alterar: inicio
-es = 1;
-imax = 20;
-% nao alterar: fim
+  % nao alterar: inicio
+  es = 1;        
+  imax = 20;     
+  % nao alterar: fim
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  t_raizes = zeros(imax,1);   
+  erro = zeros(imax,1);      
+  
+  t_raizes(1) = x0;
 
-t_raizes = zeros(imax,1);
-t_raizes(1)  = x0;
+  for ii = 1:imax-1
+      
+      [f_val, f_der] = func(t_raizes(ii));
 
-for ii = 1:length(t_raizes)-1
-    if ii ~= 1
-        erro(ii) = abs((t_raizes(ii) - t_raizes(ii-1))/t_raizes(ii))
-        if erro(ii) < es
-            break
-        endif
-    endif
-        t_raizes(ii+1) = t_raizes(ii) - func(t_raizes(ii))/func_d(t_raizes(ii));    
-    
-    
-endfor
-t = t_raizes(ii);
-    
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+      
+      t_raizes(ii+1) = t_raizes(ii) - f_val / f_der;
+
+      
+      erro(ii+1) = abs((t_raizes(ii+1) - t_raizes(ii)) / t_raizes(ii+1)) * 100;
+
+      
+      if erro(ii+1) < es
+          break;
+      end
+  endfor
+
+  
+  t = t_raizes(ii+1);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  
 endfunction
